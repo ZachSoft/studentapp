89bac33477933bb049ae1efe57588c18c889b9b9
ZACHARY  BUSHU <107484458+ZachSoft@users.noreply.github.com>
2024-10-23 18:44:14 +0300

    Updating packages
    Updating imported packages

--- app/src/main/java/com/example/studentapp/DataEntryFragment.java
+++ app/src/main/java/com/example/studentapp/DataEntryFragment.java
@@ -1,11 +1,16 @@
 package com.example.studentapp;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class DataEntryFragment extends Fragment {
