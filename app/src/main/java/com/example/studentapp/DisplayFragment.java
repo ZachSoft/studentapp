11cc080cb5b6e732dba436dec303aa0ec524ceb7
ZACHARY  BUSHU <107484458+ZachSoft@users.noreply.github.com>
2024-10-23 18:50:16 +0300

    DisplayFragment Updated
    Diplay Fragment updated and imported packages

--- app/src/main/java/com/example/studentapp/DisplayFragment.java
+++ app/src/main/java/com/example/studentapp/DisplayFragment.java
@@ -1,3 +1,15 @@
+package com.example.studentapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 public class DisplayFragment extends Fragment {
 
     private TextView textName, textAge, textGrade, textMajor;
