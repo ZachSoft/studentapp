74ce91a4344a4895728e54a29e4420a3d19699d2
ZACHARY  BUSHU <107484458+ZachSoft@users.noreply.github.com>
2024-10-24 13:49:26 +0300

    Updated display Fragment
    Display Fragment was updated

--- app/src/main/java/com/example/studentapp/DisplayFragment.java
+++ app/src/main/java/com/example/studentapp/DisplayFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,19 +14,21 @@ import androidx.fragment.app.Fragment;
 public class DisplayFragment extends Fragment {
 
     private TextView textName, textAge, textGrade, textMajor;
+    private Button goBackButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_display, container, false);
 
-        // Find TextViews
+        // Find views
         textName = view.findViewById(R.id.display_name);
         textAge = view.findViewById(R.id.display_age);
         textGrade = view.findViewById(R.id.display_grade);
         textMajor = view.findViewById(R.id.display_major);
+        goBackButton = view.findViewById(R.id.go_back_btn);
 
-        // Get data from Bundle
+        // Get data from arguments passed by MainActivity
         Bundle bundle = getArguments();
         if (bundle != null) {
             textName.setText("Name: " + bundle.getString("name"));
@@ -33,6 +36,12 @@ public class DisplayFragment extends Fragment {
             textGrade.setText("Grade: " + bundle.getString("grade"));
             textMajor.setText("Major: " + bundle.getString("major"));
         }
+
+        // Handle "Go Back" button click
+        goBackButton.setOnClickListener(v -> {
+            // Use FragmentManager to navigate back to DataEntryFragment
+            requireActivity().getSupportFragmentManager().popBackStack();
+        });
 
         return view;
     }
