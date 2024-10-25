1fb1da07976f5851e6f8fc2d7ecfecd3c87f4594
ZACHARY  BUSHU <107484458+ZachSoft@users.noreply.github.com>
2024-10-25 11:14:46 +0300

    Final code restructuring
    Final code restructuring and enhancing

--- app/src/main/java/com/example/studentapp/DisplayFragment.java
+++ app/src/main/java/com/example/studentapp/DisplayFragment.java
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.studentapp.StudentAdapter;
 
 import java.util.ArrayList;
 
