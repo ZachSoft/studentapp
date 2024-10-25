4efc6741d2f96de6c16f99ef012cf1db5f847f8a
ZACHARY  BUSHU <107484458+ZachSoft@users.noreply.github.com>
2024-10-25 10:57:07 +0300

    Update Student Adapter
    Update Student Adapter with components

--- app/src/main/java/com/example/studentapp/StudentAdapter.java
+++ app/src/main/java/com/example/studentapp/StudentAdapter.java
@@ -1,4 +1,51 @@
 package com.example.studentapp;
 
-public class StudentAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.ViewHolder> {
+
+    private ArrayList<Bundle> studentDataList;
+
+    public StudentAdapter(ArrayList<Bundle> studentDataList) {
+        this.studentDataList = studentDataList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.student_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Bundle studentData = studentDataList.get(position);
+        holder.textName.setText("Name: " + studentData.getString("name"));
+        holder.textAge.setText("Age: " + studentData.getString("age"));
+        holder.textGrade.setText("Grade: " + studentData.getString("grade"));
+        holder.textMajor.setText("Major: " + studentData.getString("major"));
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentDataList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textName, textAge, textGrade, textMajor;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textName = itemView.findViewById(R.id.display_name);
+            textAge = itemView.findViewById(R.id.display_age);
+            textGrade = itemView.findViewById(R.id.display_grade);
+            textMajor = itemView.findViewById(R.id.display_major);
+        }
+    }
 }
